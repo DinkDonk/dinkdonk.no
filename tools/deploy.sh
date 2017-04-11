4c67fad05f9e0df5e1b2b7f38b0e13e3ff7ffdcf
Rune Warhuus <rune@dinkdonk.no>
2017-04-11 14:25:53 +0200

    Automatic creation of CNAME file

---
+++ tools/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd "${BASH_SOURCE%/*}/../" || exit
+
+npm run build
+
+git checkout -b gh-pages
+git add -f build
+git commit -m 'Deploy to gh-pages'
+git push origin `git subtree split --prefix build`:gh-pages --force
+git checkout master
+git branch -D gh-pages
